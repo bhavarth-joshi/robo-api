29a7fdcba0d2b7d7c9844ec609dd395c58d02409
Bhavarth Joshi <bhavarthjoshi916@yahoo.com>
2024-04-12 19:22:53 +0530

    Adding drf-writable-nested

--- core/testai/serializers.py
+++ core/testai/serializers.py
@@ -1,27 +1,21 @@
 from rest_framework import serializers
+from drf_writable_nested.serializers import WritableNestedModelSerializer
 from testai.models import Tests, TestCase, TestStep
 
 class TestStepSerializer(serializers.ModelSerializer):
     class Meta:
         model = TestStep
-        fields = ['description'] # 'id', 'created', 'testcase', 
+        fields = ['description'] #  'created', 'testcase', 
 
-class TestCaseSerializer(serializers.ModelSerializer):
-    steps = serializers.SlugRelatedField(
-        many=True,
-        read_only=True,
-        slug_field='description'
-    )
+class TestCaseSerializer(WritableNestedModelSerializer):
+    steps = TestStepSerializer(many=True)
     
     class Meta:
         model = TestCase
         fields = ['title', 'steps'] # 'id', 'created', 'test',
 
-class TestsSerializer(serializers.ModelSerializer):
-    tests = TestCaseSerializer(
-        many=True,
-        read_only=True
-    )
+class TestsSerializer(WritableNestedModelSerializer):
+    tests = TestCaseSerializer(many=True)
     
     class Meta:
         model = Tests
--- core/testai/urls.py
+++ core/testai/urls.py
@@ -6,8 +6,8 @@ from testai import views
 # Create a router and register our ViewSets with it.
 router = DefaultRouter()
 router.register(r'tests', views.TestViewSet, basename='tests')
-router.register(r'testcase', views.TestCaseViewSet, basename='testcase')
-router.register(r'teststep', views.TestStepViewSet, basename='teststep')
+# router.register(r'testcase', views.TestCaseViewSet, basename='testcase')
+# router.register(r'teststep', views.TestStepViewSet, basename='teststep')
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
